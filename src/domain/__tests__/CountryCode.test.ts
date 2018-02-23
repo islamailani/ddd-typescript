279b9652f0e9863841562aad0c3e5358dbcd6739
Rick Lancee <rickondraw@gmail.com>
2018-02-23 09:50:36 +0100

    add country codes

---
+++ src/domain/__tests__/CountryCode.test.ts
@@ -0,0 +1,26 @@
+
+import { CountryCode } from '../access/CountryCode'
+import { IllegalArgumentException } from '../IllegalArgumentException'
+
+it('should throw when passing in a invalid country code', () => {
+    const createCountryCode = () => {
+        new CountryCode('foo')
+    }
+
+    expect(createCountryCode).toThrowError('"foo" is not a valid Alpha2 country');
+    expect(createCountryCode).toThrowError(IllegalArgumentException);
+})
+
+it('should equal an email with the same value.', () => {
+    const code1 = new CountryCode('nl')
+    const code2 = new CountryCode('nl')
+
+    expect(code1.equals(code2)).toBe(true)
+})
+
+it('should not equal an email with a different value.', () => {
+    const code1 = new CountryCode('nl')
+    const code2 = new CountryCode('us')
+
+    expect(code1.equals(code2)).toBe(false)
+})
---
+++ src/domain/access/CountryCode.ts
@@ -0,0 +1,25 @@
+import { isValid, getAlpha2Codes } from 'i18n-iso-countries'
+import { ValueObject } from '../ValueObject'
+import { IllegalArgumentException } from '../IllegalArgumentException'
+
+export class CountryCode implements ValueObject {
+
+    private alpha2: string
+
+    constructor(alpha2: string) {
+
+        if (!isValid(alpha2) || !(alpha2.toUpperCase() in getAlpha2Codes())) {
+            throw new IllegalArgumentException(`"${alpha2}" is not a valid Alpha2 country`)
+        }
+
+        this.alpha2 = alpha2
+    }
+
+    getAlpha2() {
+        return this.alpha2
+    }
+
+    equals(other: CountryCode) {
+        return this.alpha2 === other.alpha2
+    }
+}
